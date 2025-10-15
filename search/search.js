47a70d05c3f115189d2a6a21c5b9f5ee5686ee13
Eduin Contreras <jereofficial@gmail.com>
2025-10-14 23:14:54 -0400

    cambios al javascrip y comienxo del filtro

--- search/search.js
+++ search/search.js
@@ -1,6 +1,50 @@
-const article = document.querySelector(".Article-container");
+// filtro de javascript
+function filtro() {
+    const tecnoChange = tecnologia.value;
+    const ubicacionChange = ubicacion.value;
+    const tipoChange = tipo.value;
+    const experienciaChange = experiencia.value;
 
-article.addEventListener("click", () => {
-    
-})
+    workUbication.forEach((work) => {
+        const tecnologiaWork = work.getAttribute("data-tecnologia");
+        const ubicacionWork = work.getAttribute("data-ubicacion");
+        const tipoWork = work.getAttribute("data-tipo");
+        const experienciaWork = work.getAttribute("data-experiencia");
+        if (tecnologiaWork === tecnoChange.toLowerCase()) {
+            work.style.display = "block";
+        } else {
+            work.style.display = "none";
+        }
+    });
+}
+// obtener valor de los select
+const tecnologia = document.querySelector("#tecnologia");
+const ubicacion = document.querySelector("#ubicacion");
+const tipo = document.querySelector("#tipo");
+const experiencia = document.querySelector("#experiencia");
 
+//obtener informacion de los trabajos
+const workUbication = document.querySelectorAll(".typeOrUbication");
+
+
+tecnologia.addEventListener("change", () => {
+   
+   workUbication.forEach((work) => {
+      if (work.textContent.toLowerCase() === tecnoChange.toLowerCase()) {
+         work.style.display = "block";
+      } else {
+         work.style.display = "none";
+      }
+   });
+});
+
+ubicacion.addEventListener("change", () => {
+    console.log("ubicacion", ubicacion.value);
+});
+
+experiencia.addEventListener("change", () => {
+    console.log("experiencia", experiencia.value);
+});
+tipo.addEventListener("change", () => {
+    console.log("tipo", tipo.value);
+});
